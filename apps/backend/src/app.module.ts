cd2e40a8f91d0e36c94ccf996543612b1a80f93f
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2023-12-10 16:52:03 +0500

    remove user controller

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UserController } from './user/user.controller';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController, UserController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
