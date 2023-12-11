fc440b8506fb2a6a1d0fc7d1a94789f950edce7d
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2023-12-11 12:24:46 +0500

    Add the username & a default image when register

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 type User = {
+  username: string;
   email: string;
   password: string;
-  image: string;
+  image?: string;
 };
 
 @Injectable()
@@ -12,6 +13,10 @@ export class AuthService {
   constructor(private _prisma: PrismaService) {}
 
   async create(user: User) {
+    // creating a random profile image
+    const image = `https://ui-avatars.com/api/?name=${user.username}`;
+    // assigning that to the user object
+    user.image = image;
     return this._prisma.user.create({
       data: user,
     });
