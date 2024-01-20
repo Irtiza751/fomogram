d1b1c4ab4f535cb41428f67a237a4d2cc483abdd
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-01-20 14:39:01 +0500

    setup register page

--- apps/client/src/app/(auth)/login/page.tsx
+++ apps/client/src/app/(auth)/login/page.tsx
@@ -8,6 +8,12 @@ export default function Login() {
         <h2 className="text-2xl font-bold mb-5">Login in to your account</h2>
         <Input label="Username or Email" />
         <Input label="Password" type="password" />
+        <Link
+          href="/forgot"
+          className="block text-indigo-800 text-sm text-right hover:underline font-semibold"
+        >
+          Forgot password?
+        </Link>
         <Button className="w-full">Login</Button>
         <p className="text-center">
           Don't have an account?{" "}
