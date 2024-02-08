66bcdae54ac0ffd0eb927ed98b7f61a80ee9d9e6
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-02-08 20:24:53 +0500

    remove cookies support

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,16 +2,11 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const { httpAdapter } = app.get(HttpAdapterHost);
-  app.enableCors({
-    credentials: true,
-  });
   app.setGlobalPrefix('api');
-  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   await app.listen(4000);
--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -1,13 +1,8 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get("token");
-  console.log("cookie", cookie);
-  if (!cookie?.value) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-  return NextResponse.redirect(new URL("/", request.url));
 }
 
 export const config = {
