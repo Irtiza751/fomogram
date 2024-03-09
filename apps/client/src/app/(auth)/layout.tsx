0f7bb5c698c284056e74b92b3ab09dfc2b77c222
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-03-09 12:42:38 +0500

    Update page meta title

--- apps/client/src/app/(auth)/layout.tsx
+++ apps/client/src/app/(auth)/layout.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Fomogram | Login",
+};
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
