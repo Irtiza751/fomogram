ea14fa59e35d2975669e5eea2875dad6e8b860b0
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-03-10 16:35:44 +0500

    Remove unnecesary code

--- apps/client/src/app/(auth)/forgot/page.tsx
+++ apps/client/src/app/(auth)/forgot/page.tsx
@@ -3,11 +3,6 @@ import Link from "next/link";
 import { Button, Input } from "@fomogram/ui";
 import { useFormik } from "formik";
 import { InferType, object, string } from "yup";
-import { useRouter } from "next/navigation";
-import { Eye, EyeOff, XCircle } from "react-feather";
-import { useState } from "react";
-import { fomo } from "@client/api/fomo";
-import { AxiosError } from "axios";
 
 const forgotSchema = object({
   email: string().email("Invalid email").required("Email is required"),
@@ -16,10 +11,6 @@ const forgotSchema = object({
 type Email = InferType<typeof forgotSchema>;
 
 export default function Forgot() {
-  const [show, setShow] = useState(false);
-  const [error, showError] = useState<string>("");
-  const router = useRouter();
-
   const onSubmit = async (email: Email) => {
     console.log(email);
   };
@@ -37,16 +28,6 @@ export default function Forgot() {
       onSubmit={handleSubmit}
       className="border bg-white rounded w-full max-w-md px-6 py-8 m-auto space-y-4"
     >
-      {/* error */}
-      {error ? (
-        <div className="bg-red-500 text-white py-2 px-4 rounded-md flex items-center justify-between">
-          <span>{error}</span>
-          <button onClick={() => showError("")}>
-            <XCircle size={20} />
-          </button>
-        </div>
-      ) : null}
-
       <h2 className="text-2xl font-bold mb-5">Forgot your password?</h2>
       <Input
         name="email"
