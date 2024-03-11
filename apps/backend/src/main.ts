2b67ccf47ea1ac082ebbdcd2c3c1921bf1e51fcb
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-03-11 13:20:12 +0500

    Changes

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,17 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
-// const allowedOrigins = [
-//   'http://localhost:3000',
-//   'https://fomogram.netlify.app',
-//   'https://fomogram.vercel.app',
-// ];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://fomogram.netlify.app',
+  'https://fomogram.vercel.app',
+];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.enableCors({
-    origin: true,
+    origin: allowedOrigins,
     credentials: true,
   });
   app.use(cookieParser());
