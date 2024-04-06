fa017ee635b40c0cc67904a23ef6cb8aa51e784f
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-04-06 16:20:01 +0500

    backend post image uploading done

---
+++ apps/backend/src/cloudinary/cloudinary.ts
@@ -0,0 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+export const Cloudinary = {
+  provide: 'CLOUDINARY',
+  useFactory() {
+    return cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.CLOUD_API_KEY,
+      api_secret: process.env.CLOUD_SECRET,
+    });
+  },
+};
