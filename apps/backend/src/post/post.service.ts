d5e571172755eba65c52d7d3b41974af81a490f8
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-04-07 01:39:31 +0500

    Saving the image while creating a new post

--- apps/backend/src/post/post.service.ts
+++ apps/backend/src/post/post.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 export interface Post {
   userId: number;
-  image: string;
+  image?: string;
   caption: string;
 }
 
 @Injectable()
 export class PostService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly cloudinary: CloudinaryService,
+  ) {}
 
-  async create(post: Post) {
+  async create(post: Post, file?: Express.Multer.File) {
+    if (file) {
+      const fileRes = await this.cloudinary.upload(file);
+      post.image = fileRes.url;
+    }
+
     return await this.prisma.post.create({
       data: post,
     });
@@ -21,6 +30,7 @@ export class PostService {
     /**
      * @todo Fetch posts of followings & followers
      */
+    console.log(userId);
     return await this.prisma.post.findMany({
       orderBy: { createdAt: 'desc' },
       include: { user: { select: { image: true, username: true } } },
