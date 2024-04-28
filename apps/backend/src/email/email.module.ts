1f1b18faea08333371ddaad9d7d417fc4fc6da0c
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-04-28 14:43:03 +0500

    test

--- apps/backend/src/email/email.module.ts
+++ apps/backend/src/email/email.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { MailgunModule } from 'nestjs-mailgun';
 
 @Module({
   providers: [EmailService],
   exports: [EmailService],
+  imports: [
+    MailgunModule.forRoot({
+      username: 'api',
+      key: 'string',
+      public_key: 'string', // OPTIONAL
+      url: 'string',
+    }),
+  ],
 })
 export class EmailModule {}
