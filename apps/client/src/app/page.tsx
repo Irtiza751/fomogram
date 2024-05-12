866f7ba211bf2f5ba8c141839d6307b4c4c23912
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-05-12 21:02:17 +0500

    Minor changes

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,13 +1,42 @@
 "use client";
 import Navbar from "@client/components/Navbar";
-import { Posts } from "@client/components/Posts";
+// import { Posts } from "@client/components/Posts";
+import { Post, Skeleton } from "@client/components/Post";
+import { useFetch } from "@client/hooks/useFetch";
+
+export interface Post {
+  id: number;
+  userId: number;
+  caption: string;
+  image: string;
+  createdAt: string;
+  updatedAt: string;
+  user: User;
+  likes: Array<{ userId: number }>;
+}
+
+export interface User {
+  image: string;
+  username: string;
+}
 
 export default function Home() {
+  const { data: posts, isLoading } = useFetch<Post[]>({
+    endpoint: "/post/allposts",
+  });
+
   return (
     <>
       <Navbar />
       <main className="max-w-xl mx-auto">
-        <Posts />
+        {isLoading ? (
+          <>
+            <Skeleton />
+            <Skeleton />
+          </>
+        ) : (
+          posts?.map((post) => <Post key={post.id} post={post} />)
+        )}
       </main>
     </>
   );
--- apps/client/src/components/Post.tsx
+++ apps/client/src/components/Post.tsx
@@ -75,3 +75,22 @@ export function Post({ post }: { post: PostInterface }) {
     </div>
   );
 }
+
+export const Skeleton = () => {
+  return (
+    <div className="rounded-md p-4 w-full mx-auto mb-5">
+      <div className="animate-pulse flex space-x-4">
+        <div className="rounded-full bg-slate-200 h-10 w-10"></div>
+        <div className="flex-1 space-y-6 py-1">
+          <div className="space-y-3">
+            <div className="h-2 bg-slate-200 rounded"></div>
+            <div className="grid grid-cols-3 gap-4">
+              <div className="h-2 bg-slate-200 rounded col-span-2"></div>
+            </div>
+          </div>
+          <div className="h-96 bg-slate-200 rounded"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
