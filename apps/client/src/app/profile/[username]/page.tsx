d598573c4f7be1196d46ed9652445996c1a5ebeb
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-06-08 15:30:41 +0500

    Integrate user profile page

--- apps/client/src/app/profile/[username]/page.tsx
+++ apps/client/src/app/profile/[username]/page.tsx
@@ -3,11 +3,16 @@
 import Image from "next/image";
 import Navbar from "@client/components/Navbar";
 import ProfileGroup from "@client/components/Followers";
-import { Button } from "@fomogram/ui";
+import { Button, Dialog, Input, Spinner, Textarea } from "@fomogram/ui";
 // import { Metadata } from "next";
 import { useFetch } from "@client/hooks/useFetch";
 import { Profile as ProfileType } from "./types";
 import { ProfileTabs } from "@client/components/ProfileTabs";
+import { useState } from "react";
+import { InferType, object, string } from "yup";
+import { useFormik } from "formik";
+import { useRequest } from "@client/hooks/useRequest";
+import { ButtonClickEvent, UploadEvent } from "@client/types/events";
 
 // export const metadata: Metadata = {
 //   title: "Fomogram | Profile",
@@ -28,10 +33,61 @@ const followers = [
   },
 ];
 
+const profileSchema = object({
+  username: string().required("username is required"),
+  bio: string().max(255, { message: "Max length is 255" }),
+});
+
+type ProfileForm = InferType<typeof profileSchema>;
+
 export default function Profile() {
+  const [showEdit, setShowEdit] = useState(false);
+  const [image, setImage] = useState<File>();
+
+  const { post, isLoading } = useRequest({
+    endpoint: "/user/update",
+    onSuccess(res) {
+      console.log(res);
+    },
+  });
+
+  const { values, errors, touched, handleSubmit, handleChange, setValues } =
+    useFormik({
+      initialValues: {
+        username: "",
+        bio: "",
+      },
+      onSubmit: async (payload: ProfileForm) => {
+        const formData = new FormData();
+        formData.append("username", payload.username);
+        formData.append("bio", payload?.bio || "");
+        formData.append("image", image || "");
+
+        // console.log(Object.fromEntries(formData.entries()));
+        await post(payload);
+      },
+      validationSchema: profileSchema,
+    });
+
   const { data } = useFetch<ProfileType>({
     endpoint: "/user/profile",
+    onSuccess(data) {
+      setValues({ username: data.username || "", bio: data.bio || "" });
+    },
   });
+
+  const onProfileUpload = (e: UploadEvent) => {
+    const { files } = e.target;
+
+    if (files) {
+      setImage(files[0]);
+    }
+  };
+
+  const removeImage = (e: ButtonClickEvent) => {
+    e.preventDefault();
+    setImage(undefined);
+  };
 
   return (
     <>
@@ -56,9 +112,62 @@ export default function Profile() {
 
             <ProfileGroup followers={followers} />
 
-            <Button variant="outline" className="w-full">
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={() => setShowEdit(true)}
+            >
               Edit Profile
             </Button>
+
+            <Dialog show={showEdit} onClose={() => setShowEdit(false)}>
+              <form
+                className="bg-white px-6 py-4 rounded-xl"
+                onSubmit={handleSubmit}
+              >
+                <h3 className="font-semibold mb-3">Edit profile</h3>
+                <div>
+                  <input
+                    type="file"
+                    className="hidden"
+                    id="file"
+                    name="image"
+                    onChange={onProfileUpload}
+                  />
+                  {image ? (
+                    <Image
+                      className="rounded-full mb-3 outline-2"
+                      src={URL.createObjectURL(image)}
+                      alt={image.name}
+                      width={80}
+                      height={80}
+                    />
+                  ) : null}
+                  <Button type="button" className="w-full" variant="outline">
+                    <label htmlFor="file" className="w-full cursor-pointer">
+                      Upload Picture
+                    </label>
+                  </Button>
+                </div>
+                <Input
+                  type="text"
+                  label="Username"
+                  name="username"
+                  onChange={handleChange}
+                  value={values.username}
+                />
+                <Textarea
+                  label="Bio"
+                  name="bio"
+                  placeholder="You bio"
+                  onChange={handleChange}
+                  value={values.bio}
+                />
+                <Button type="submit" className="w-full py-2 mt-4">
+                  {isLoading ? <Spinner size={24} /> : "Update"}
+                </Button>
+              </form>
+            </Dialog>
 
             <ProfileTabs />
           </>
