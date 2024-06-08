9f7619354e90c219f71eb5af44e88da43a94a555
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-06-08 15:34:23 +0500

    Make the API for user profile update

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -6,31 +6,43 @@ import {
   Query,
   UseGuards,
   Request,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { FollowDto } from './dtos/follow.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { RequestObj } from 'src/post/post.controller';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('user')
+@UseGuards(AuthGuard)
 export class UserController {
   constructor(private readonly userSrv: UserService) {}
 
-  @UseGuards(AuthGuard)
   @Get('/search')
   search(@Query('search') search: string, @Request() req: RequestObj) {
     return this.userSrv.searchUser(search, req.user.id);
   }
 
-  @UseGuards(AuthGuard)
   @Post('/follow')
   follow(@Body() data: FollowDto) {
     return this.userSrv.addFollower(data);
   }
 
-  @UseGuards(AuthGuard)
   @Get('/profile')
   profile(@Request() req: RequestObj) {
     return this.userSrv.userProfile(req.user.id);
+  }
+
+  @Post('/update')
+  @UseInterceptors(FileInterceptor('image'))
+  update(
+    @Request() req: RequestObj,
+    @Body() profile: UpdateUserDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    return this.userSrv.updateProfile(profile, req.user.id, image);
   }
 }
--- apps/backend/src/user/user.module.ts
+++ apps/backend/src/user/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, CloudinaryModule],
 })
 export class UserModule {}
