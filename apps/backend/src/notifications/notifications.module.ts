83b8c62cbfe44ee557490fc3fd98f40418c0e33d
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-07-01 15:00:32 +0500

    Remove the seperate notification service

--- apps/backend/src/notifications/notifications.module.ts
+++ apps/backend/src/notifications/notifications.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { NotificationsService } from './notifications.service';
 import { NotificationsGateway } from './notifications.gateway';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  providers: [NotificationsService, NotificationsGateway],
+  providers: [NotificationsGateway],
   imports: [PrismaModule],
 })
 export class NotificationsModule {}
