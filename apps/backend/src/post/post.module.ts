b93b6ba2ca5dd72ef1e516d4eda9e67eacaf0c88
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-07-01 15:04:01 +0500

    Imported the notification gateway service in the post

--- apps/backend/src/post/post.module.ts
+++ apps/backend/src/post/post.module.ts
@@ -4,10 +4,12 @@ import { PostService } from './post.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { NotificationsGateway } from 'src/notifications/notifications.gateway';
+import { RedisService } from 'src/redis/redis.service';
 
 @Module({
   controllers: [PostController],
-  providers: [PostService],
+  providers: [PostService, NotificationsGateway, RedisService],
   imports: [PrismaModule, AuthModule, CloudinaryModule],
 })
 export class PostModule {}
