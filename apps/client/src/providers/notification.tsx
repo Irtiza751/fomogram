1f9f5bebdaecb07eab41520799fef18cf69e5203
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-07-07 00:22:13 +0500

    connection socket after logging in.

--- apps/client/src/providers/notification.tsx
+++ apps/client/src/providers/notification.tsx
@@ -1,21 +1,57 @@
 "use client";
 
-import { createContext, useEffect } from "react";
-import io from "socket.io-client";
+import { createContext, useEffect, useState } from "react";
+import io, { Socket } from "socket.io-client";
+
+type NotificationType =
+  | "comment_post"
+  | "like_post"
+  | "new_post"
+  | "new_follower";
+
+export type Notification = {
+  type: NotificationType;
+  message: string;
+};
 
 type SocketProps = { children: React.ReactNode; userId?: string };
 
-export const SocketContext = createContext({});
+export const SocketContext = createContext<{
+  notification?: Notification;
+  connect: (id: string) => void;
+} | null>(null);
 
 export function SocketProvider({ children, userId }: SocketProps) {
+  const [socket, setSocket] = useState<Socket>();
+  const [notification, setNotification] = useState<Notification>();
+
+  const connect = (userId: string) => {
+    if (userId) {
+      setSocket(
+        io(process.env.NEXT_PUBLIC_SOCKET_URL || "", {
+          query: {
+            userId: +window.atob(userId),
+          },
+        })
+      );
+    }
+  };
+
   useEffect(() => {
     if (userId) {
-      const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "", {
-        query: {
-          userId: +window.atob(userId),
-        },
-      });
+      setSocket(
+        io(process.env.NEXT_PUBLIC_SOCKET_URL || "", {
+          query: {
+            userId: +window.atob(userId),
+          },
+        })
+      );
+    }
+  }, []);
 
+  useEffect(() => {
+    console.log("connecting....");
+    if (socket) {
       socket.on("connect", () => {
         console.log("connected!");
       });
@@ -25,16 +61,21 @@ export function SocketProvider({ children, userId }: SocketProps) {
       });
 
       socket.on("onNotification", (data) => {
+        setNotification(data);
         console.log("Notification received:", data);
       });
-
-      return () => {
-        socket.off("connect");
-        socket.off("connect_error");
-        socket.off("onNotification");
-      };
     }
-  }, [userId]);
 
-  return <SocketContext.Provider value={{}}>{children}</SocketContext.Provider>;
+    return () => {
+      socket?.off("connect");
+      socket?.off("connect_error");
+      socket?.off("onNotification");
+    };
+  }, [socket]);
+
+  return (
+    <SocketContext.Provider value={{ notification, connect }}>
+      {children}
+    </SocketContext.Provider>
+  );
 }
